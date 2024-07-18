91fbeda9917b6d9428670f8152e592f021557ba1
Rachael Babister <rachaelbabister@gmail.com>
2024-07-18 15:57:36 +0000

    FEAT: Add App and NavBar css styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,26 @@
-import React from 'react'
-import {Container, Nav, Navbar, NavLink} from "react-bootstrap";
-import logo from "../assets/FromHouseToHome-logo.jpg"
+import React from "react";
+import { Container, Nav, Navbar, NavLink } from "react-bootstrap";
+import logo from "../assets/FromHouseToHome-logo.jpg";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
-    return (
-        <Navbar expand="md" fixed="top">
-            <Container>
-                <NavLink to="/">
-                    <Navbar.Brand>
-                        <img src={logo} alt="logo" height="100" />
-                    </Navbar.Brand>
-                </NavLink>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto text-right">
-                        <Nav.Link><i className="fa-solid fa-house"></i> Home</Nav.Link>
-                        <Nav.Link><i className="fa-solid fa-right-to-bracket"></i> Sign in</Nav.Link>
-                        <Nav.Link><i className="fa-solid fa-user-plus"></i> Sign up</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
-}
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height="100" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-right">
+            <Nav.Link>Home</Nav.Link>
+            <Nav.Link><i className="fa-solid fa-right-to-bracket"></i> Sign in</Nav.Link>
+            <Nav.Link><i className="fa-solid fa-user-plus"></i> Sign up</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default NavBar;
