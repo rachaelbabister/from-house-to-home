aa6f3f381889387e82ef324dc1799267a9e5bfdc
Rachael Babister <rachaelbabister@gmail.com>
2024-07-25 18:26:00 +0000

    FEAT: Create SignUpForm and sign up/in and button css styles. Amend app and navbar styles to match colour styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
 import './api/axiosDefaults';
+import SignUpForm from "./pages/auth/SignUpForm";
 
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
                 <Switch>
                     <Route exact path="/" render={() => <h1>Home</h1>} />
                     <Route path="/signin" render={() => <h1>Sign In</h1>} />
-                    <Route path="/signup" render={() => <h1>Sign Up</h1>} />
+                    <Route path="/signup" render={() => <SignUpForm />} />
                     <Route path="*" render={() => <h1>Page Not Found</h1>} />
                 </Switch>
             </Container>
---
+++ src/pages/auth/SignUpForm.js
@@ -0,0 +1,138 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+
+import styles from "../../styles/SignUpInForm.module.css";
+import btnStyles from "../../styles/Buttons.module.css";
+import appStyles from "../../App.module.css";
+
+import {
+    Form,
+    Button,
+    Col,
+    Row,
+    Container,
+    Alert,
+} from "react-bootstrap";
+import axios from "axios";
+
+const SignUpForm = () => {
+    const [signUpData, setSignUpData] = useState({
+        username: "",
+        password1: "",
+        password2: "",
+    });
+    const { username, password1, password2 } = signUpData;
+
+    const [errors, setErrors] = useState({});
+
+    const history = useHistory();
+
+    const handleChange = (event) => {
+        setSignUpData({
+            ...signUpData,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post("/dj-rest-auth/registration/", signUpData);
+            history.push("/signin");
+        } catch (err) {
+            setErrors(err.response?.data);
+        }
+    };
+
+    return (
+        <Container className={styles.Container}>
+            <Row className={`my-auto`}>
+                <Col className="my-auto py-2 p-md-2 mx-auto" md={8}>
+                    <Container className={`${appStyles.Content} p-4`}>
+                        <h2 className={styles.Header}>Create your account</h2>
+
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group className="mb-3" controlId="username">
+                                <Form.Label className="sr-only">
+                                    Username
+                                </Form.Label>
+                                <Form.Control
+                                    className={styles.Input}
+                                    type="text"
+                                    placeholder="Username"
+                                    name="username"
+                                    value={username}
+                                    onChange={handleChange}
+                                />
+                            </Form.Group>
+                            {errors.username?.map((message, idx) => (
+                                <Alert variant="warning" key={idx}>
+                                    {message}
+                                </Alert>
+                            ))}
+
+                            <Form.Group className="mb-3" controlId="password1">
+                                <Form.Label className="sr-only">
+                                    Password
+                                </Form.Label>
+                                <Form.Control
+                                    className={styles.Input}
+                                    type="password"
+                                    placeholder="Password"
+                                    name="password1"
+                                    value={password1}
+                                    onChange={handleChange}
+                                />
+                            </Form.Group>
+                            {errors.password1?.map((message, idx) => (
+                                <Alert variant="warning" key={idx}>
+                                    {message}
+                                </Alert>
+                            ))}
+
+                            <Form.Group className="mb-3" controlId="password2">
+                                <Form.Label className="sr-only">
+                                    Confirm Password
+                                </Form.Label>
+                                <Form.Control
+                                    className={styles.Input}
+                                    type="password"
+                                    placeholder="Confirm Password"
+                                    name="password2"
+                                    value={password2}
+                                    onChange={handleChange}
+                                />
+                            </Form.Group>
+                            {errors.password2?.map((message, idx) => (
+                                <Alert variant="warning" key={idx}>
+                                    {message}
+                                </Alert>
+                            ))}
+                            {errors.non_field_errors?.map((message, idx) => (
+                                <Alert
+                                    variant="warning"
+                                    className="mt-3"
+                                    key={idx}>
+                                    {message}
+                                </Alert>
+                            ))}
+
+                            <Button
+                                className={`${btnStyles.Button} ${btnStyles.Wide}`}
+                                type="submit">
+                                Sign up
+                            </Button>
+                        </Form>
+                    </Container>
+                    <Container className={`mt-3 ${appStyles.Content}`}>
+                        <Link className={styles.Link} to="/signin">
+                            Already have an account? <span>Sign in</span>
+                        </Link>
+                    </Container>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default SignUpForm;
