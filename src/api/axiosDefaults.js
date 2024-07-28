686d57b71fd37b4bf7e72c8f755c7c9d04c6aff8
Rachael Babister <rachaelbabister@gmail.com>
2024-07-28 16:39:16 +0000

    FIX: Bug with not being able to create a new post, so updated json packages and checked axiosDefaults for any bugs

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://home-api-58bb6b7692c8.herokuapp.com';
+axios.defaults.baseURL = 'https://home-api-58bb6b7692c8.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
