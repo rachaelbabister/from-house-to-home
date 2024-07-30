1b488d5e0935b09739af709f267f38fc5e5e7130
Rachael Babister <rachaelbabister@gmail.com>
2024-07-30 11:33:15 +0000

    FEAT: Creat PostPage.js and add route to App.js. FIX: Update node and npm versions to work with react-scripts to fix parse error

---
+++ src/pages/posts/PostPage.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Col, Row, Container } from "react-bootstrap";
+
+import appStyles from "../../App.module.css";
+
+function PostPage() {
+
+
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <p>Popular profiles for mobile</p>
+        <p>Post component</p>
+        <Container className={appStyles.Content}>Comments</Container>
+      </Col>
+      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+        Popular profiles for desktop
+      </Col>
+    </Row>
+  );
+}
+
+export default PostPage;
