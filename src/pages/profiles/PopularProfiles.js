a9a2323458c1e98e5f573551ef4c757eea96f34e
Rachael Babister <rachaelbabister@gmail.com>
2024-07-31 08:12:23 +0000

    FEAT: Create ProfileDataContext.js and update PopularProfiles and index.js files accordingly

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,44 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Container } from "react-bootstrap";
 
-import { axiosReq } from "../../api/axiosDefaults";
-import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import appStyles from "../../App.module.css";
 import Asset from "../../components/Asset";
 import Profile from "./Profile";
+import { useProfileData } from "../../contexts/ProfileDataContext";
 
 const PopularProfiles = ({ mobile }) => {
-    const [profileData, setProfileData] = useState({
-        // will use pageProfile here
-        pageProfile: { results: [] },
-        popularProfiles: { results: [] },
-    });
-    const { popularProfiles } = profileData;
-    const currentUser = useCurrentUser();
-
-    useEffect(() => {
-        const handleMount = async () => {
-            try {
-                const { data } = await axiosReq.get(
-                    "/profiles/?ordering=-followers_count"
-                );
-                setProfileData((prevState) => ({
-                    ...prevState,
-                    popularProfiles: data,
-                }));
-            } catch (err) {
-                console.log(err);
-            }
-        };
-
-        handleMount();
-    }, [currentUser]);
+    const { popularProfiles } = useProfileData();
 
     return (
-        <Container className={appStyles.Content}>
+        <Container
+            className={`${appStyles.Content} ${
+                mobile && "d-lg-none text-center mb-3"
+            }`}>
             {popularProfiles.results.length ? (
                 <>
-                    <p>Most followed profiles</p>
+                    <p>Most followed profiles.</p>
                     {mobile ? (
                         <div className="d-flex justify-content-around">
                             {popularProfiles.results
