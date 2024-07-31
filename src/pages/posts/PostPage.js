ebc582e0d94f51a1c9863318afc00be009de281b
Rachael Babister <rachaelbabister@gmail.com>
2024-07-31 03:50:08 +0000

    FEAT: Create Comment.js and Comment.module.css to enable commenting

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -4,9 +4,10 @@ import { Col, Row, Container } from "react-bootstrap";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
-import Post from "./Post";
-import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Post from "./Post";
+import Comment from "../comments/Comment";
+import CommentCreateForm from "../comments/CommentCreateForm";
 
 function PostPage() {
     const { id } = useParams();
@@ -19,11 +20,12 @@ function PostPage() {
     useEffect(() => {
         const handleMount = async () => {
             try {
-                const [{ data: post }] = await Promise.all([
+                const [{ data: post }, { data: comments }] = await Promise.all([
                     axiosReq.get(`/posts/${id}`),
+                    axiosReq.get(`/comments/?post=${id}`),
                 ]);
                 setPost({ results: [post] });
-                console.log(post);
+                setComments(comments);
             } catch (err) {
                 console.log(err);
             }
@@ -49,6 +51,15 @@ function PostPage() {
                     ) : comments.results.length ? (
                         "Comments"
                     ) : null}
+                    {comments.results.length ? (
+                        comments.results.map((comment) => (
+                            <Comment key={comment.id} {...comment} />
+                        ))
+                    ) : currentUser ? (
+                        <span>No comments yet, be the first to comment!</span>
+                    ) : (
+                        <span>No comments... yet</span>
+                    )}
                 </Container>
             </Col>
             <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
